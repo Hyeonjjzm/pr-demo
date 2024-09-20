bab51d1f3597b9fa67f3b4fc7e91aea345ad9484
hyeonjjzm <hdjsjy0504@gmail.com>
2024-09-20 14:11:39 +0900

    09_20

--- pages/school.js
+++ pages/school.js
@@ -1,7 +1,7 @@
 import { useEffect } from "react";
 export default function School(){
 useEffect(() => {
-    window.location.href = "/school/index.html"
+    window.location.href = "/school/test.html"
 })
     return null;
 }
